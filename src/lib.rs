420d59e4f395ecfb9ed7e879ccbcc5f92ed386ad
Johan Kristell <johan@jott.se>
2020-01-08 08:51:45 +0100

    Update to embedded-hal pin v2

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,14 @@
 #![deny(missing_docs)]
 #![deny(warnings)]
 #![no_std]
-#![feature(never_type)]
 
 extern crate embedded_hal as hal;
 
 #[macro_use(block)]
 extern crate nb;
 
-use hal::digital::InputPin;
-use hal::digital::OutputPin;
+use hal::digital::v2::InputPin;
+use hal::digital::v2::OutputPin;
 
 /// Maximum ADC value
 pub const MAX_VALUE: i32 = (1 << 23) - 1;
@@ -30,42 +29,41 @@ pub struct Hx711<IN, OUT> {
     mode: Mode,
 }
 
-impl<IN, OUT> Hx711<IN, OUT>
+impl<IN, OUT, PINERR> Hx711<IN, OUT>
 where
-    IN: InputPin,
-    OUT: OutputPin,
+    IN: InputPin<Error=PINERR>,
+    OUT: OutputPin<Error=PINERR>,
 {
     /// Creates a new driver from Input and Outut pins
-    pub fn new(dout: IN, mut pd_sck: OUT) -> Self {
-        pd_sck.set_low();
-        let mut hx711 = Hx711 {
+    pub fn new(dout: IN, pd_sck: OUT) -> Self {
+        Hx711 {
             dout,
             pd_sck,
             mode: Mode::ChAGain128,
-        };
-        hx711.reset();
-        hx711
+        }
     }
 
     /// Set the mode (channel and gain).
-    pub fn set_mode(&mut self, mode: Mode) {
+    pub fn set_mode(&mut self, mode: Mode) -> Result<(), PINERR>{
         self.mode = mode;
-        block!(self.retrieve()).unwrap();
+        block!(self.retrieve())?;
+        Ok(())
     }
 
     /// Reset the chip. Mode is Channel A Gain 128 after reset.
-    pub fn reset(&mut self) {
-        self.pd_sck.set_high();
+    pub fn reset(&mut self) -> Result<(), PINERR> {
+        self.pd_sck.set_high()?;
         for _ in 1..3 {
-            self.dout.is_high();
+            self.dout.is_high()?;
         }
-        self.pd_sck.set_low();
+        self.pd_sck.set_low()?;
+        Ok(())
     }
 
     /// Retrieve the latest conversion value if available
-    pub fn retrieve(&mut self) -> nb::Result<i32, !> {
-        self.pd_sck.set_low();
-        if self.dout.is_high() {
+    pub fn retrieve(&mut self) -> nb::Result<i32, PINERR> {
+        self.pd_sck.set_low()?;
+        if self.dout.is_high()? {
             // Conversion not ready yet
             return Err(nb::Error::WouldBlock);
         }
@@ -74,9 +72,9 @@ where
         for _ in 0..24 {
             // Read 24 bits
             count <<= 1;
-            self.pd_sck.set_high();
-            self.pd_sck.set_low();
-            if self.dout.is_high() {
+            self.pd_sck.set_high()?;
+            self.pd_sck.set_low()?;
+            if self.dout.is_high()? {
                 count += 1;
             }
         }
@@ -84,8 +82,8 @@ where
         // Continue to set mode for next conversion
         let n_reads = self.mode as u16;
         for _ in 0..n_reads {
-            self.pd_sck.set_high();
-            self.pd_sck.set_low();
+            self.pd_sck.set_high()?;
+            self.pd_sck.set_low()?;
         }
 
         Ok(i24_to_i32(count))
